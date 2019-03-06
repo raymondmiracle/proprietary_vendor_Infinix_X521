680b05d2acb3707a6d1030fc0b1884399a8cb305
raymondmiracle <rmiracle201@gmail.com>
2019-03-06 09:07:08 +0000

    first commit

---
+++ X521-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X521/vendor,system/vendor)
